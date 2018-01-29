2de6c20be29edde201f0023d4118a4ac1d79bd8c
edwin rivera <waehake@gmail.com>
2018-01-29 17:20:34 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class ThemedDecorations extends React.Component{
+export default class ThemedDecorations extends React.Component {
   render(){
 
     const childTheme = 'heaven';
