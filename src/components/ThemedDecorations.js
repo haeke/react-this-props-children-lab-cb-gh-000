59bce6cfff783b38cf81e392482a27f74a3d8a53
edwin rivera <waehake@gmail.com>
2018-01-29 17:16:29 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,19 @@
 // Code ThemedDecoration Component Here
+import React, {Component} from 'react';
+
+class ThemedDecoration extends Component {
+  render() {
+    const theme = 'party';
+    const themeItems = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: theme
+        });
+      }
+    );
+    return (
+        {themeItems}
+      );
+  }
+}
+
+export default ThemedDecoration;
