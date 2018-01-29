790366ce484d7124510a22449c467717495e84f5
edwin rivera <waehake@gmail.com>
2018-01-29 17:26:30 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 
 class ThemedDecorations extends Component {
   render() {
-    const childtheme = 'heaven';
     const themeItems = React.Children.map(this.props.children, child => {
       return <div className={this.props.theme}>{child}</div>
     });
